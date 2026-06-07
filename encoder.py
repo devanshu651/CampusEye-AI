b4ac2255af2c8bfdc70fbdd26583d8d8996efdb3
Devanshu Raut <rautdevanshu24@gmail.com>
2026-06-08 00:47:36 +0530

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -10,15 +10,15 @@ def encode_students(directory='student_db/'):
 
     if not os.path.exists(directory):
         os.makedirs(directory)
-        print(f"📁 Created {directory}.")
+        print(f"Created {directory}.")
         return
 
-    print("🚀 Scanning & Standardizing Images...")
+    print("Scanning and processing images...")
 
     for filename in os.listdir(directory):
         if filename.lower().endswith((".jpg", ".png", ".jpeg")):
             path = os.path.join(directory, filename)
-            
+
             try:
                 pil_img = Image.open(path).convert('RGB')
                 img = np.array(pil_img, dtype='uint8')
@@ -28,21 +28,25 @@ def encode_students(directory='student_db/'):
                     known_encodings.append(encodings[0])
                     metadata = os.path.splitext(filename)[0]
                     known_metadata.append(metadata)
-                    print(f"✅ Success: {metadata}")
+                    print(f"Success: {metadata}")
                 else:
-                    print(f"⚠️ No face found in: {filename}")
+                    print(f"No face found in: {filename}")
 
             except Exception as e:
-                print(f"❌ Error in {filename}: {str(e)}")
+                print(f"Error in {filename}: {str(e)}")
 
     if known_encodings:
-        data = {"encodings": known_encodings, "metadata": known_metadata}
+        data = {
+            "encodings": known_encodings,
+            "metadata": known_metadata
+        }
+
         with open("encodings.p", "wb") as f:
             pickle.dump(data, f)
-        print("\n Encodings saved successfully to 'encodings.p'")
+
+        print("\nEncodings saved successfully to 'encodings.p'")
     else:
-        print("\n Encoding failed. Please check the photos again.")
+        print("\nEncoding failed. Please check the photos again.")
 
 if __name__ == "__main__":
-
     encode_students()
